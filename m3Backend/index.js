3b929d8c299e4542041a7b557f3ef8adf325f3c0
rhyzJ <rhyannonjohns@gmail.com>
2024-12-01 22:10:20 +1300

    fixed error on server run (commented out unused routes) and installed nodemon again

--- m3Backend/index.js
+++ m3Backend/index.js
@@ -4,25 +4,25 @@ require("dotenv").config();
 const app = express();
 
 // import routes
-const route = require("./routes/routeName");
+// const route = require("./routes/routeName");
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // use = router for a specific path
-app.use("/api", route);
+// app.use("/api", route);
 
 // ----------ENDPOINTS------------ //
 
 // root
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("connected my dudes 🔌");
 });
 
 // catch for any undefined routes
 app.use((req, res) => {
-  res.status(404).send("Endpoint not found");
+  res.status(404).send("endpoint not found");
 });
 
 // set up port
