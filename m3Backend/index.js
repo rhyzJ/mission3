d79a6055bff318bd802e02eedeaca8dedcea7f83
rhyzJ <rhyannonjohns@gmail.com>
2024-12-01 22:03:12 +1300

    added index.js defualt page

---
+++ m3Backend/index.js
@@ -0,0 +1,32 @@
+const cors = require("cors");
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+// import routes
+const route = require("./routes/routeName");
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// use = router for a specific path
+app.use("/api", route);
+
+// ----------ENDPOINTS------------ //
+
+// root
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+// catch for any undefined routes
+app.use((req, res) => {
+  res.status(404).send("Endpoint not found");
+});
+
+// set up port
+const PORT = process.env.SERVER_PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
