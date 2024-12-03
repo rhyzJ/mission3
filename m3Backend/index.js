c0d45196bda5232034124416fff890a49458eac9
rhyzJ <rhyannonjohns@gmail.com>
2024-12-04 11:07:45 +1300

    fixed middleware placement

--- m3Backend/index.js
+++ m3Backend/index.js
@@ -4,8 +4,9 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 require("dotenv").config();
 
-const app = express();
+// middleware
 app.use(cors());
+app.use(express.json());
 
 let chatSession = null;
 
@@ -70,10 +71,6 @@ app.post("/api/interview", async (req, res) => {
     res.status(500).json({ error: "Failed to get AI response." });
   }
 });
-
-// middleware
-app.use(cors());
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Connected my dudes 🔌");
